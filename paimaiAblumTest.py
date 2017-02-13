adee86653a1c905b05250fc57702e1a7190b027c
lianmengjun <lianmengjun@gmail.com>
2017-02-13 23:08:33 +0800

    完整版工具抓取

--- paimaiAblumTest.py
+++ paimaiAblumTest.py
@@ -1,58 +1,49 @@
 import re, bs4, requests
 
 
-def data(item):
-    for i in range(0, len(item)):
-        id = item[i].get('id')
-        itemIdRe = re.compile(r'''(pmp-item-)(\d+)''')
-        itemId = itemIdRe.search(id).group(2)
-        print('id:' + itemId)
+print('please input the sellID (like 2355097081):')
+sellID = input()
+# sellID = '2355097081'
 
-        title = item[i].select('a[title]')
-        itemTitleRe = re.compile(r'''title="(.+)"''')
-        itemTitle = itemTitleRe.search(str(title)).group(1)
-        print('title:' + itemTitle)
-
-        price = item[i].select('span b')
-        priceRe = re.compile(r'''<b>(.+)</b>.*<b(.+)</b>.*<b>(.+)</b>''')
-        itemPrice = priceRe.search(str(price)).group(3)
-        print('Price:' + itemPrice)
-
-        startTime = item[i].select('.soon-time')
-        startTimeRe = re.compile(r'''\n(.+)开拍\n''')
-        itemStartTime = startTimeRe.search(str(startTime)).group(1)
-        print('starTime:' + itemStartTime.strip())
-
-        endTime = item[i].select('span')
-        endTimeRe = re.compile(r'结束</span>, <span class="time-num">(.+)</span>, <span class="info-num">')
-        itemEndTime = endTimeRe.search(str(endTime)).group(1)
-        print('endTime:' + itemEndTime)
-
-        print()
-
-
-print('please input the sellID:')
-# sellID = input()
-sellID = '2355097081'
-
-html = 'https://paimai.taobao.com/pmp_seller/' + sellID + '.htm'
+html = 'https://paimai.taobao.com/pmp_seller/' + sellID + '_1.htm'
 
 res = requests.get(html)
 res.raise_for_status
 test = bs4.BeautifulSoup(res.text, 'html.parser')
 
-itemDateRe = re.compile(r'"data": ([\d\D]*)</script>\n'
-                        r'<script class="J_pmp_item_list_template" type="text/template">')
-itemDate = itemDateRe.search(str(test)).group(1)
+albumDateRe = re.compile(r'"data": ([\d\D]*?)</script>\n')
+albumDate = albumDateRe.search(str(test)).group(1)
 
-# print(itemDate)
-# print(test)
+albumListRe = re.compile(r'"albumId": "(.*)",([\d\D]*?)'
+                         r'"title": "(.*)",')
+albumList = albumListRe.findall(albumDate)
 
-itemRe = re.compile(r'"currentPriceFmt": "(.*)",([\d\D]*?)'
-                    r'"itemId": "(.*)",([\d\D]*?)'
-                    r'"statusDesc": "(.*)",([\d\D]*?)'
-                    r'"title": "(.*)",([\d\D]*?)')
+for i in range(0, len(albumList)):
+    print(str(i+1) +' '+albumList[i][0] + ' ' +albumList[i][2])
 
-item = itemRe.search(str(itemDate)).group(1)+itemRe.search(str(itemDate)).group(3)\
-       + itemRe.search(str(itemDate)).group(5)+itemRe.search(str(itemDate)).group(7)
-print(item)
+print('please input the ablumNum:')
+ablumNum = input()
+# ablumNum = 1
+
+albumID = albumList[ablumNum - 1][0]
+
+albumHtml = 'https://paimai.taobao.com/pmp_album/' + albumID + '.htm'
+res = requests.get(albumHtml)
+res.raise_for_status
+test = bs4.BeautifulSoup(res.text, 'html.parser')
+
+itemDate = test.select('.pmp-item-list')
+
+itemlistRe = re.compile(r'<a href="//paimai.taobao.com/pmp_item/(.*?)\.htm"(.*?)'
+                        r'title="(.*?)">(.*?)</a>([\d\D]*?)'
+                        r'起拍价</span>\n<span class="price-num"><em>¥</em><b>(.*?)</b>([\d\D]*?)'
+                        r'当前价</span>\n<span class="price-num"><em>¥</em>'
+                        r'<b class="xmpp-current-price">(.*?)</b>([\d\D]*?)')
+
+itemList = itemlistRe.findall(str(itemDate))
+
+for i in range(0, len(itemList)):
+    print(str(i + 1) + ' ' + itemList[i][0] \
+          + ' ' + itemList[i][2] \
+          + ' ' + itemList[i][5] \
+          + ' ' + itemList[i][7])
